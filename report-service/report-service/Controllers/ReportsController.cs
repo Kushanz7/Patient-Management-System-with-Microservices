51233ac8d16b45b53fff67a5211dea71d068d0fb
Kushan <kushansr7@gmail.com>
2025-07-12 22:34:56 +0530

    create report-service using .NET

---
+++ report-service/report-service/Controllers/ReportsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace report_service.Controllers
+{
+    public class ReportsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ report-service/report-service/Kafka/KafkaConsumerService.cs
@@ -0,0 +1,6 @@
+﻿namespace report_service.Kafka
+{
+    public class KafkaConsumerService
+    {
+    }
+}
